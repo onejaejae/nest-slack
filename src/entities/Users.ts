75d20dbf729f471ad29c1efac8c5d6c8579a33c1
Cho Won Jae <yoteamo7@naver.com>
2022-01-29 23:49:11 +0900

    typeorm 관계 설정하기

---
+++ src/entities/Users.ts
@@ -0,0 +1,106 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  Index,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ChannelChats } from './ChannelChats';
+import { ChannelMembers } from './ChannelMembers';
+import { Channels } from './Channels';
+import { DMs } from './DMs';
+import { Mentions } from './Mentions';
+import { WorkspaceMembers } from './WorkspaceMembers';
+import { Workspaces } from './Workspaces';
+
+@Index('email', ['email'], { unique: true })
+// name은 테이블명이다
+@Entity({ schema: 'sleact', name: 'users' })
+export class Users {
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  id: number;
+
+  @Column('varchar', { name: 'email', unique: true, length: 30 })
+  email: string;
+
+  @Column('varchar', { name: 'nickname', length: 30 })
+  nickname: string;
+
+  @Column('varchar', { name: 'password', length: 100, select: false })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+
+  @OneToMany(() => ChannelChats, (channelchats) => channelchats.User)
+  ChannelChats: ChannelChats[];
+
+  @OneToMany(() => ChannelMembers, (channelmembers) => channelmembers.User)
+  ChannelMembers: ChannelMembers[];
+
+  @OneToMany(() => DMs, (dms) => dms.Sender)
+  DMs: DMs[];
+
+  @OneToMany(() => DMs, (dms) => dms.Receiver)
+  DMs2: DMs[];
+
+  @OneToMany(() => Mentions, (mentions) => mentions.Sender)
+  Mentions: Mentions[];
+
+  @OneToMany(() => Mentions, (mentions) => mentions.Receiver)
+  Mentions2: Mentions[];
+
+  @OneToMany(
+    () => WorkspaceMembers,
+    (workspacemembers) => workspacemembers.User,
+  )
+  WorkspaceMembers: WorkspaceMembers[];
+
+  @OneToMany(() => Workspaces, (workspaces) => workspaces.Owner)
+  OwnedWorkspaces: Workspaces[];
+
+  // workspaces는 Workspaces.ts에서 설정한
+  // @Entity({ schema: 'sleact', name: 'workspaces' }) 테이블명이다
+  @ManyToMany(() => Workspaces, (workspaces) => workspaces.Members)
+  // ManyToMany 관계는 JoinTable()을 넣는다
+  // ManyToMany 관계는 JoinTable()을 둘 중에 하나만 넣으면 된다.
+  // 다대다 관계이므로 새로운 테이블이 생긴다.
+  // workspacemembers는 테이블명이다.
+  @JoinTable({
+    name: 'workspacemembers',
+    joinColumn: {
+      name: 'UserId',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'WorkspaceId',
+      referencedColumnName: 'id',
+    },
+  })
+  Workspaces: Workspaces[];
+
+  @ManyToMany(() => Channels, (channels) => channels.Members)
+  @JoinTable({
+    name: 'channelmembers',
+    joinColumn: {
+      name: 'UserId',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'ChannelId',
+      referencedColumnName: 'id',
+    },
+  })
+  Channels: Channels[];
+}
